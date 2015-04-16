a7cca515a4c83cbaf371e7f448668107afa93503
Jørgen Bergquist <gismos@gmail.com>
2015-04-16 18:38:25 +0200

    Cleaned up simple example

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -1,29 +1,30 @@
 var express = require('express'),
-    app = require('../../index')(express);
+    libby = require('../../index'),
+    app = libby(express);
 
-app.get('/', function(req, res){
-  res.render('index', { users: users });
-});
-
-app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/views');
-
-// Set our default template engine to "jade"
-// which prevents the need for extensions
-// (although you can still mix and match)
-app.set('view engine', 'jade');
-
+// Set up example data
 function User(name, email) {
   this.name = name;
   this.email = email;
 }
 
-// Dummy users
 var users = [
     new User('Arnt', 'a@example.com'),
     new User('Ove', 'o@example.com')
 ];
+
+// Express configuration
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+// Define route.
+app.get('/', function(req, res){
+  res.render('index', { users: users });
+});
+
+// Static route at end.
+app.use(express.static(__dirname + '/public'));
+
 
 app.listen(3000);
 console.log('Express app started on port 3000');
